bddc6bdd741a90766e2d971d8784b5df2caed727
Yasmini Gyawali <gyawaliyasmini246@gmail.com>
2022-10-13 10:06:14 +0545

    Learning NodeJs Day2

---
+++ resource/todo.js
@@ -0,0 +1,23 @@
+const todos = [
+    {
+        "id":"001",
+        "title":"Complete bootcamp project",
+        "description":"Talk to team, make a team repository and assign task"
+    },
+    {
+        "id":"002",
+        "title":"My Introduction",
+        "description":"Explain details about myself"
+    },
+    {
+        "id":"003",
+        "title":"Go to market",
+        "description":"buy clothes"
+    },
+    {
+        "id":"004",
+        "title":"Cooking Food",
+        "description":"Cook varieties of food and do dishes"
+    }
+]
+module.exports = todos;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const todos = require('../resource/todo.js') //importing todos
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yasmini' });
-});
-router.get('/home',function(req,res,next){
-  res.render('home',{
-    fname : 'Yasmini',
-    lname : 'Gyawali',
-    age : 20,
-    study : 'Bachelors',
-    title : 'Software Engineer'
-  })
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Yasmini' });
+// });
+router.get('/',function(req,res,next){
+  res.render('home',{todosList:todos})
 }
 )
+
+router.get('/add-to-do',function(req,res,next){
+  res.render('addTodo', {title: 'To-do'});
+}
+)
+router.post('/save-to-do', function(req,res,next){
+  todos.push({...req.body, id: `00${todos.length}`}); //spread operator -> ...
+  res.redirect('/');
+})
+// router.get('/delete-to-do/:index', function(req,res,next){
+//   console.log(req.params.index);
+//   todos.splice(req.params.index,1);
+//   res.redirect('/');
+// })
+router.get('/delete-to-do/:id',function(req,res,next){
+  const index = todos.findIndex(todo=>todo.id === req.params.id);
+  todos.splice(index,1);
+  // todos.splice(req.params.index,1);
+  res.redirect('/')
+})
+router.get('/open-update-form/:id',function(req,res,next)
+{
+  const todotodo = todos.find(todo => todo.id === req.params.id);
+  res.render('editToDo', {todo: todotodo});
+})
+router.post('/edit-to-do/:id', function(req,res,next){
+  const index = todos.findIndex(todo=>todo.id === req.params.id);
+  todos.splice(index,1, {...req.body, id: req.params.id});
+  res.redirect('/');
+})
+
 module.exports = router;
